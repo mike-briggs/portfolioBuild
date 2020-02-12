dcde0dd6ec14ea3aa01612790c0ebaa4f361f4ca
Mike Briggs <43759598+mike-briggs@users.noreply.github.com>
2020-02-12 12:07:03 -0500

    Add files via upload

---
+++ precache-manifest.1c7b20b9056795156e481371ee5ae0bb.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2409acff8995aa609c12a995e44d497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db148298580a72767f6f",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "b61ec094640ca1987084",
+    "url": "/static/css/main.bb4e6ebf.chunk.css"
+  },
+  {
+    "revision": "db148298580a72767f6f",
+    "url": "/static/js/2.434845be.chunk.js"
+  },
+  {
+    "revision": "b65beae02b0662f7446e8a9ad1c28945",
+    "url": "/static/js/2.434845be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61ec094640ca1987084",
+    "url": "/static/js/main.0ba39535.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "15084ab62c40d8b69efd20c3b8275da6",
+    "url": "/static/media/btc-side.15084ab6.png"
+  },
+  {
+    "revision": "9593daa4535b65fc0c35c77b6a2f956b",
+    "url": "/static/media/drone.9593daa4.png"
+  },
+  {
+    "revision": "36476f96127391590c2bc85ddce64f7a",
+    "url": "/static/media/fiscalvid.36476f96.mp4"
+  },
+  {
+    "revision": "ef9f07dc6e1b3cbaea09b65a2753190d",
+    "url": "/static/media/food-mobile.ef9f07dc.jpg"
+  },
+  {
+    "revision": "1ee2ed860d41492be6abcc6dace485a3",
+    "url": "/static/media/food.1ee2ed86.jpg"
+  },
+  {
+    "revision": "6f3be0a52096cd487569a37eb5ffaf3e",
+    "url": "/static/media/home.6f3be0a5.png"
+  },
+  {
+    "revision": "18bea4e68fc2bef46ec97d9360c0abfa",
+    "url": "/static/media/logo.18bea4e6.svg"
+  },
+  {
+    "revision": "a1b2c4fd56992e9997d88fa6b9038d2c",
+    "url": "/static/media/node.a1b2c4fd.png"
+  },
+  {
+    "revision": "2367001e314e36ce65d1acd06a87fb8b",
+    "url": "/static/media/react-white.2367001e.png"
+  },
+  {
+    "revision": "4a883f0ab562fbec23c1d894812db37c",
+    "url": "/static/media/react.4a883f0a.png"
+  },
+  {
+    "revision": "c12813e1bb43515d1943851468c07896",
+    "url": "/static/media/safe_dentistry.c12813e1.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6cdbfad08f94eb048fc7ca569cc803fd",
+    "url": "/static/media/space_cutout.6cdbfad0.png"
+  }
+]);
